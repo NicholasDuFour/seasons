0f89b9ab1754ef1aa8939ff3faa7c69d2b8a470e
nick <nmdufour@gmail.com>
2019-01-21 19:55:50 -0500

    Updates

---
+++ precache-manifest.b49d5647574e1e6e82a6646b1ec024d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "0aa85fe4bcafda7dfa9d",
+    "url": "/seasons/static/js/main.0aa85fe4.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/seasons/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "0aa85fe4bcafda7dfa9d",
+    "url": "/seasons/static/css/main.5d18afee.chunk.css"
+  },
+  {
+    "revision": "16654ec3cb815117aa07846aad48344f",
+    "url": "/seasons/index.html"
+  }
+];
